f9379f98f23fd92ef45480af3fd27aa3125718d0
Axetroy <axetroy.dev@gmail.com>
2022-03-06 00:34:07 +0800

    add test

--- src/git.rs
+++ src/git.rs
@@ -17,9 +17,16 @@ pub fn url_to_path(root: &str, url: &str) -> Result<PathBuf, Report> {
         Ok(r) => {
             let mut dir = PathBuf::new();
 
+            let host = r.host.expect("invalid repository host");
+            let owner = r.owner.expect("invalid repository owner");
+
+            if host.is_empty() || owner.is_empty() {
+                return Err(Report::msg("url host is empty"));
+            }
+
             dir.push(root);
-            dir.push(r.host.expect("invalid repository host"));
-            dir.push(r.owner.expect("invalid repository owner"));
+            dir.push(host);
+            dir.push(owner);
             dir.push(r.name);
 
             let abs = dir.absolutize()?;
@@ -203,6 +210,37 @@ mod tests {
         #[cfg(target_family = "windows")]
         let result1: &str =
             &(cwd.as_os_str().to_str().unwrap().to_owned() + &"\\gitlab.com\\sub_org\\gpm.rs");
+
+        assert_eq!(p1.as_os_str().to_str().unwrap(), result1)
+    }
+
+    #[test]
+    fn test_url_to_path_with_invalud_url() {
+        let url1 = "https://gitlab.com/gpm.rs";
+
+        let r1 = git::url_to_path(".", url1);
+
+        assert!(r1.is_err());
+    }
+
+    #[test]
+    fn test_url_to_path_with_ssh() {
+        let url1 = "git@github.com:axetroy/gpm.rs.git";
+
+        let r1 = git::url_to_path(".", url1);
+
+        assert!(!r1.is_err());
+        assert!(r1.is_ok());
+
+        let p1 = r1.ok().unwrap();
+        let cwd = env::current_dir().unwrap();
+
+        #[cfg(target_family = "unix")]
+        let result1: &str =
+            &(cwd.as_os_str().to_str().unwrap().to_owned() + &"/github.com/axetroy/gpm.rs");
+        #[cfg(target_family = "windows")]
+        let result1: &str =
+            &(cwd.as_os_str().to_str().unwrap().to_owned() + &"\\github.com\\axetroy\\gpm.rs");
 
         assert_eq!(p1.as_os_str().to_str().unwrap(), result1)
     }
