a076781a76adbd2438433f6e41eeece8ba140637
Axetroy <axetroy.dev@gmail.com>
2022-03-06 01:00:33 +0800

    v0.1.6

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Preset {
 
 fn main() {
     let mut app = Command::new("gpm")
-        .version("v0.1.5")
+        .version("v0.1.6")
         .author("Axetroy <axetroy.dev@gmail.com>")
         .about("A command line tool, manage your hundreds of repository, written with Rust")
         .subcommand_required(true)
