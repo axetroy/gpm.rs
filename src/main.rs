1ecf2d6045a61924f42dd7c70ca794ade715613d
Axetroy <axetroy.dev@gmail.com>
2022-03-09 00:10:46 +0800

    v0.1.11

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     let config_field_root = PossibleValue::new("root").help("The root of clones repository");
 
     let mut app = Command::new("gpm")
-        .version("v0.1.10")
+        .version("v0.1.11")
         .author("Axetroy <axetroy.dev@gmail.com>")
         .about("A command line tool, manage your hundreds of repository, written with Rust")
         .subcommand_required(true)
